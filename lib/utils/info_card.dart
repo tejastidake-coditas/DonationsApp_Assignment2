ddca67a64d984bad22be1bb7c5b1758bfb2d6b85
Tejas Tidake <tejas.tidake@coditas.com>
2025-02-22 00:57:08 +0530

    Minor Improvements in Card

--- lib/utils/info_card.dart
+++ lib/utils/info_card.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-class InfoCard extends StatefulWidget {
+class InfoCard extends StatelessWidget {
   const InfoCard({super.key});
 
-  @override
-  State<InfoCard> createState() => _InfoCardState();
-}
-
-class _InfoCardState extends State<InfoCard> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
@@ -15,42 +10,77 @@ class _InfoCardState extends State<InfoCard> {
         children: [
           Row(
             children: [
-              Flexible(flex:2, child: Image.network("https://www.cry.org/wp-content/uploads/why-donation-matters-for-child-education-min.jpg")),
-              SizedBox(width: 10,),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: Image.network(
+                  "https://www.cry.org/wp-content/uploads/why-donation-matters-for-child-education-min.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(width: 18),
               Flexible(
                 flex: 4,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Education for All", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
-                    Text("Education Foundation", style: TextStyle(fontWeight: FontWeight.w500, color: Colors.black38, fontSize: 13.5),),
-                    SizedBox(height: 6,),
-                    LinearProgressIndicator(value: 0.5, color: Color(0xFA0FA685), backgroundColor: Colors.grey[300], minHeight: 10, borderRadius: BorderRadius.circular(10),),
-                    SizedBox(height: 8,),
+                    Text(
+                      "Education for All",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    Text(
+                      "Education Foundation",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500, color: Colors.black38, fontSize: 13.5),
+                    ),
+                    SizedBox(height: 6),
+                    LinearProgressIndicator(
+                      value: 0.5,
+                      color: Color(0xFA0FA685),
+                      backgroundColor: Colors.grey[300],
+                      minHeight: 10,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    SizedBox(height: 8),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Container(
-                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(14),
-                            color: Color(0xFAF0F4F4),),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(14),
+                            color: Color(0xFAF0F4F4),
+                          ),
                           child: Padding(
                             padding: const EdgeInsets.fromLTRB(5, 3, 5, 3),
-                            child: Text(r"$766.950", style: TextStyle(color: Color(
-                                0xFA0FA685), fontSize: 15, fontWeight: FontWeight.bold),),
+                            child: Text(
+                              r"$766.950",
+                              style: TextStyle(
+                                  color: Color(0xFA0FA685),
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                            ),
                           ),
                         ),
-                        Text("50 days left", style: TextStyle(fontWeight: FontWeight.w700, color: Colors.black54, fontSize: 14),)
-                        ],
-                    )
+                        Text(
+                          "50 days left",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700, color: Colors.black54, fontSize: 14),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
             ],
           ),
-          SizedBox(height: 14,),
-          Divider(height: 20, thickness: 2, color: Colors.black12,)
+          SizedBox(height: 14),
+          Divider(height: 20, thickness: 2, color: Colors.black12),
         ],
-      )
+      ),
     );
   }
 }
